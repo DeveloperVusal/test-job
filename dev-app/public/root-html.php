11b3810acb30eee1d6d344bdb18d5d748830925d
unknown <developer.vusal96@gmail.com>
2020-07-07 08:10:00 +0300

    Настройка react

---
+++ dev-app/public/root-html.php
@@ -0,0 +1,15 @@
+<?php echo __FILE__;?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Single Page Application</title>
+</head>
+<body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div class="root"></div>
+    <script type="text/javascript" src="./dev-app/src/index.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello world</h1>
-</body>
-</html>
+<?php
+require_once('dev-app/public/root-html.php')
+?>
